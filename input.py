519d2003e039ef003a49ff868e5d07f8c2f96c34
Aarti12-bca <bca22092@gmail.com>
2025-08-19 19:08:43 +0530

    Varible Input

---
+++ input.py
@@ -0,0 +1,26 @@
+# input the all the datatype in python
+#integer data type input
+
+age=int(input("Enter The Your Age : "))
+
+#here the print varible
+
+print("Your age is : ",age)
+
+#input of the string data type 
+
+name=input("Enter Your Name : ")
+
+# print your name
+
+print("Your name is : ",name)
+
+# input of float data type 
+
+# here i am the take randomly varible
+
+price=float(input("Enter the Price : "))
+
+# print float varible
+
+print("Your Price is : ",price)
